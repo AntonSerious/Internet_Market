781d061d36bd2e3912308c5343374f1dac8e36a7
anemchenko <anemchenko@nes.ru>
2021-07-04 17:25:44 +0300

    first commit

---
+++ ANemchenko-iMarket/src/main/java/com/anemchenko/ANemchenkoIMarketApplication.java
@@ -0,0 +1,13 @@
+package com.anemchenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ANemchenkoIMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ANemchenkoIMarketApplication.class, args);
+	}
+
+}
---
+++ ANemchenko-iMarket/src/main/java/com/anemchenko/MainController.java
@@ -0,0 +1,4 @@
+package com.anemchenko;
+
+public class MainController {
+}
