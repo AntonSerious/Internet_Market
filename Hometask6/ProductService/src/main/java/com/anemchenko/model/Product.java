334e904ee05e55911426d7fb38c8361f141ba793
anemchenko <anemchenko@nes.ru>
2021-11-27 01:23:36 +0300

    hometask6

---
+++ Hometask6/ProductService/src/main/java/com/anemchenko/model/Product.java
@@ -0,0 +1,37 @@
+package com.anemchenko.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Product_Id")
+    private Long productId;
+
+    @Column(name = "Title")
+    private String title;
+
+    @Column(name = "Price")
+    private BigDecimal price;
+
+    @CreationTimestamp
+    @Column(name = "Created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "Modified_at")
+    private LocalDateTime modifiedAt;
+
+}
