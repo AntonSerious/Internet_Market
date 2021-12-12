cb4256dc07ba5a67fa870bb450f28b1850c7fe34
anemchenko <anemchenko@nes.ru>
2021-12-12 17:36:57 +0300

    hometask2_7

---
+++ ANemchenko-iMarket/src/test/java/com/anemchenko/SecurityTest.java
@@ -0,0 +1,40 @@
+package com.anemchenko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SecurityTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void CheckUnathorizedUserTest() throws Exception {
+        mockMvc
+                .perform(
+                        get("/api/v1/orders")
+                )
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+    @Test
+    @WithMockUser(username = "user1", roles = "USER")
+    public void CheckAthorizedUserTest() throws Exception {
+        mockMvc
+                .perform(
+                        get("/api/v1/orders")
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
